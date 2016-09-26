feffeb62e797f2af38ac3a595586fd881ac30cab
Anthosbadguy <jesusmarinalv@gmail.com>
2016-09-26 15:16:16 -0400

    Correcciones y comentarios
    Aquí te adjunto los cambios y correcciones, presta atención a los
    comentarios en estos dos archivos (main y components/filter/input.jsx) y
    a la forma en la que uso setState dentro de un setState para queuear
    state changes sin tanta complicación.

--- components/Filter/input.jsx
+++ components/Filter/input.jsx
@@ -7,7 +7,13 @@ export default class Input extends React.Component {
     }
     render() {
 
-    	
+    	/*Nota:
+    	No uses functionName() en los inline html event props (onChange, onClick...) ya que no funciona
+    	cuando se ejecute el evento, sino que se ejecuta inmediatamente cuando el componente es montado,
+    	actualizado, o demás.
+    	usa  functionName a secas, y manéjate con los argumentos dentro de la definición de la función
+
+    	*/
         return (
             <input type="text" onChange={console.log("yey")} ></input>
             )
--- main.jsx
+++ main.jsx
@@ -8,44 +8,59 @@ import Picklist from './components/Filter/picklist.jsx';
 
 class Reports extends React.Component { //Reports es un React.Component
 
-    constructor() { 
-        super();
+    constructor(props) { 
+        super(props);
 var currentTime = new Date().getFullYear();
-this.state={filters:[{date1: currentTime-1,date2:currentTime,account:"",assigned:""}] };
+this.state={date1: currentTime-1, date2:currentTime, account:"",assigned:"",recordArray:this.props.unfiltered};
+this.filterObjArray = this.filterObjArray.bind(this);
+this.setfilter = this.setfilter.bind(this);
+}
 
-function filterObjArray(obj){
 
-    if (this.state.filters.account==null){
-    this.state.filters.account
-    }
-return obj.date >=this.state.filters.date1 &&
-       obj.date <=this.state.filters.date2 &&
-       obj.account == this.state.filters.account &&
-       obj.assigned == this.state.filters.assigned ;
+
+filterObjArray(){
+
+        var newArray= this.props.unfiltered.filter(function(value){
+
+            var correctedDate= new Date(value.date).getFullYear();
+            var conditionArray=[];
+
+            conditionArray.push((correctedDate>=this.state.date1? true:false));
+            conditionArray.push((correctedDate<=this.state.date2)? true:false);
+            if(this.state.account!=""){ conditionArray.push((this.state.account==value.account)? true:false);}
+            if(this.state.assigned!=""){ conditionArray.push((this.state.assigned==value.assigned)? true:false);}
+                   
+                   
+                    return  conditionArray.every(function(condition){ return condition; });
+
+        },this);
+
+return newArray;
+}
  
-}
-
-}
-
-        
+      
 setfilter(value){
 
-    this.setState(value);
+    this.setState(value, function(){
+        this.setState({recordArray:this.filterObjArray()});
+    });
 }
 
     render() {
 
-
+//Una observación aquí: no uses <span>From <Input/></span>, haz que el componente Input incluya el span
+//Para eso son los presentational components, o "dumb" components, para que hagas markup render,
+//Así que tu Input es el que devolveria "<span>From  <input type="text" onChange={console.log("yey")} ></input></span>"
+//Y así te evitas sobrecargar este método render de tu componente principal
 
         return <div>
-
         <span>From <Input/></span>
        <span>To<Input/></span>
         <Picklist/>
         <Picklist/>     
         <Graph/> 
         <hr/>
-        <TableContainer hrow={salesOrdersNames} rowdata={this.props.unfiltered}/>
+        <TableContainer hrow={salesOrdersNames} rowdata={this.state.recordArray}/>
         </div>;
     }
 }
