5a36ac7e0854d8ef63ef86563b5e4d69451a1d24
Anthosbadguy <jesusmarinalv@gmail.com>
2016-10-03 14:14:37 -0400

    Added Status picklist
    Small fixes
    Added "status" picklist
    --Added all the change handlers and the workflowstatus state property

--- main.jsx
+++ main.jsx
@@ -13,7 +13,7 @@ class Reports extends React.Component {
 var currentTime = new Date();
 
 
-this.state={date1: "", date2:"", account:"",assigned:"",recordArray:this.props.unfiltered};
+this.state={date1: "", date2:"", account:"",assigned:"", WorkflowStatus:"",recordArray:this.props.unfiltered};
 this.filterObjArray = this.filterObjArray.bind(this);
 this.setfilter = this.setfilter.bind(this);
 }
@@ -22,22 +22,22 @@ this.setfilter = this.setfilter.bind(this);
 
 filterObjArray(){
 
-        if(this.state.date1===""){
+        if(this.state.date1==="" && this.state.date2==="" && this.state.account==="" && this.state.assigned==="" && this.state.WorkflowStatus==="" ){
+            console.log("no filters!");
             return this.props.unfiltered;
         }
 
         var newArray= this.props.unfiltered.filter(function(value){
 
             var correctedDate= new Date(value.date).toISOString();
-             var correctedDate1= new Date(this.state.date1).toISOString();
-             var correctedDate2= new Date(this.state.date2==""?new Date():this.state.date2).toISOString();
             var conditionArray=[];
 
 
-             if(this.state.date1!==""){conditionArray.push((correctedDate>=correctedDate1)? true:false);}
-           if(this.state.date2!==""){conditionArray.push((correctedDate<=correctedDate2)? true:false);} 
+             if(this.state.date1!==""){conditionArray.push((correctedDate>=(new Date(this.state.date1).toISOString()))? true:false);}
+           if(this.state.date2!==""){conditionArray.push((correctedDate<=(new Date(this.state.date2).toISOString()))? true:false);} 
             if(this.state.account!==""){ conditionArray.push((this.state.account===value.account)? true:false);}
             if(this.state.assigned!==""){ conditionArray.push((this.state.assigned===value.assigned)? true:false);}
+             if(this.state.WorkflowStatus!==""){ conditionArray.push((this.state.WorkflowStatus===value.WorkflowStatus)? true:false);}
                   
                   console.log(conditionArray);
                     return  conditionArray.every(function(condition){ return condition; });
@@ -50,7 +50,6 @@ return newArray;
 
 setfilter(value){
     this.setState(value, function(){
-        //console.log(this.state.account);
 
         this.setState({recordArray:this.filterObjArray()}, function(){
 
@@ -63,15 +62,16 @@ setfilter(value){
 
     render() {
 
-//Despues meto cada style como el style de row en custom.css zzZZzzz
         return <div className="container-responsive">
                  <div className="row" style={{margin:"15px"}} >
                     <div className="col-md-4"><Input className="datepicker" filter={"From"} id={"date1"} setfilter={this.setfilter}/></div>
                      <div className="col-md-4"><Input className="datepicker" filter={"To"} id={"date2"} setfilter={this.setfilter}/></div>
                  </div>
                  <div className="row" style={{margin:"15px"}}>
-                    <div className="col-md-4"><Picklist filter={"Account"} id={"account"} setfilter={this.setfilter} picklistdata={this.state.recordArray}/></div>
-                    <div className="col-md-4"><Picklist filter={"Assigned To"} id={"assigned"} setfilter={this.setfilter} picklistdata={this.state.recordArray} />    </div> 
+                    <div className="col-md-2"><Picklist filter={"Account"} id={"account"} setfilter={this.setfilter} picklistdata={this.state.recordArray}/></div>
+                    <div className="col-md-2"><Picklist filter={"Assigned To"} id={"assigned"} setfilter={this.setfilter} picklistdata={this.state.recordArray} /></div> 
+                    <div className="col-md-2"><Picklist filter={"Status"} id={"WorkflowStatus"} setfilter={this.setfilter} picklistdata={this.state.recordArray} /></div>
+                 
                  </div>
                 <div className="col-lg-12" style={{margin:"15px"}}>
                     <TableContainer className="table-hover table-striped table-bordered" hrow={salesOrdersNames} rowdata={this.state.recordArray}/>
