2e16572492be43c4077d24aa9a41c3b948c878ab
marinjar94 <marinjar94@gmail.com>
2016-10-04 17:22:02 -0400

    Pagination

--- components/Table/tablecontainer.jsx
+++ components/Table/tablecontainer.jsx
@@ -1,22 +1,25 @@
 ﻿import React from 'react';
 import Table from './table.jsx';
+import TableTotals from './tabletotals.jsx';
 import Controls from './controls.jsx';
 
 
 export default class TableContainer extends React.Component {
    constructor(props) {
         super(props);
-	this.state={page:2,pagination:2};
+
+this.state={page:2,pagination:2,length:this.props.rowdata.length};
 this.totalamount=this.totalamount.bind(this);
 this.totalsingle=this.totalsingle.bind(this);
 this.totalaccumulated=this.totalaccumulated.bind(this);
 this.pageinc=this.pageinc.bind(this);    
+this.paginationChange=this.paginationChange.bind(this);
 }   
 
 totalamount(){
 
 var total=0;	
-this.props.rowdata.map(function(value){
+return this.props.rowdata.map(function(value){
     var id="amount";
     for (var objectprop in value){
         if(objectprop==id ){
@@ -31,46 +34,58 @@ this.props.rowdata.map(function(value){
 
 totalaccumulated(){
 
-var total=0;
+var totalac=0;
 var counter=this.state.page*this.state.pagination;
 var rowdatasliced=this.props.rowdata.slice(0,counter);
-rowdatasliced.map(function(value){
+return rowdatasliced.map(function(value){
     var id="amount";
     for (var objectprop in value){
         if(objectprop==id ){
-        total=total+parseInt(value[objectprop]);}
+        totalac=totalac+parseInt(value[objectprop]);}
         console.log("Acc"+total);
 } 
 
-        				return  total;})
+        				return  totala;})
 
 }
 totalsingle(){
 
-var total=0;
+var totals=0;
 var counter=this.state.page*this.state.pagination;
 var rowdatasliced=this.props.rowdata.slice(counter-this.state.pagination,counter);
-rowdatasliced.map(function(value){
+return rowdatasliced.map(function(value){
     var id="amount";
     for (var objectprop in value){
         if(objectprop==id ){
-        total=total+parseInt(value[objectprop]);}
+        totals=totals+parseInt(value[objectprop]);}
         console.log("single"+total);
 } 
 
-        				return  total;})
+        				return  totals;})
                         
 
 }
 
 pageinc(event){
-if(event.target.id=="+"){
-this.state.page++;
+    if(event.target.id=="+"){
+
+    this.setState(function(previousState, currentProps) {
+        return {page: previousState.page + 1};
+        });
+        if (this.state.page==Math.ceil(this.state.length/this.state.pagination)){
+            this.setState(function(currentProps) {
+            return {page: Math.ceil(this.state.length/this.state.pagination)};
+            });
+    }
 console.log("increased" +this.state.page);
 }else{
-  this.state.page--;
-  if (this.state.page==0){
-      this.state.page=1;
+ this.setState(function(previousState, currentProps) {
+  return {page: previousState.page - 1};
+});
+  if (this.state.page==1){
+     this.setState(function(currentProps) {
+  return {page: 1};
+});
       console.log("no te pases manito" + this.state.page);
   }
     console.log("Decreased" +this.state.page);
@@ -78,13 +93,31 @@ console.log("increased" +this.state.page);
 
 }
 
+paginationChange(event){
+
+    this.setState(function() {
+        return {pagination: event.target.value};
+        });
+        if (this.state.pagination>this.state.length){
+            this.setState(function() {
+            return {pagination:this.state.length};
+            });
+    }
+console.log(this.state.pagination);
+}
+
+componentWillReceiveProps(){
+
+    this.setState(function(){return{length:this.props.rowdata.length}})
+   
+}
+
         render() {
-            
             return (<div>
             
-            <Controls page={this.state.page} pageinc={this.pageinc} pagination={this.state.pagination}/>
-            <Table className={this.props.className} hrow={this.props.hrow} rowdata={this.props.rowdata}/>
-            
+           <div> <Controls page={this.state.page} pageinc={this.pageinc} pagination={this.state.pagination} paginationChange={this.paginationChange}/> paginationtotal={this.state.length} </div>
+        <div> <Table className={this.props.className} hrow={this.props.hrow} rowdata={this.props.rowdata}/></div>
+            <div><TableTotals className={this.props.className} rowdata={[this.totalacc,this.totalamount,this.totalsingle]} /></div>
             </div>)
     }
 
---
+++ components/Table/tabletotals.jsx
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import Cell from './cell.jsx';
+import Row from './row.jsx';
+
+
+export default class TableTotals extends React.Component {
+   constructor() {
+        super();     
+	
+    }
+        render() {
+
+        return(
+        	<table className={this.props.className} >
+        <thead>
+		<tr>
+<td>Total</td>
+<td>Page</td>
+<td>Accumulated</td>
+</tr>
+		</thead>
+                	<tbody >
+                	<Row ObjectArray={this.props.rowdata}/>
+         
+                	</tbody>
+                </table>)
+    }
+
+}
